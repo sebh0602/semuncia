506198a7458fba2d2b870131ab30519df569ddce
Sebastian Hietsch <hietschseb@outlook.com>
2020-09-16 23:55:30 +0200

    Add ability to import data

--- html/js/semunciaMain.js
+++ html/js/semunciaMain.js
@@ -7,17 +7,48 @@ function init(){
 		localData.config = {
 			currentDisplay:"statsDisplay"
 		};
-		saveLocalData();
 	}
+	if (localStorage.transactions != undefined){
+		localData.transactions = JSON.parse(localStorage.transactions);
+	}
+	if (localStorage.initialAmount != undefined){
+		localData.initialAmount = JSON.parse(localStorage.initialAmount);
+	}
+	if (localStorage.recurringTransactions != undefined){
+		localData.recurringTransactions = JSON.parse(localStorage.recurringTransactions);
+	}
+	saveLocalData();
 	switchDisplay(localData.config.currentDisplay);
 }
 
 function saveLocalData(){
+	localStorage.transactions = JSON.stringify(localData.transactions);
+	localStorage.initialAmount = JSON.stringify(localData.initialAmount);
+	localStorage.recurringTransactions = JSON.stringify(localData.recurringTransactions);
 	localStorage.config = JSON.stringify(localData.config);
 }
 
 function importJSON(){
-	alert("import");
+	var reader = new FileReader();
+	reader.onload = function(event){
+		var result = JSON.parse(event.target.result);
+		if ("transactions" in result){
+			localData.transactions = result.transactions;
+		}
+		if ("initialAmount" in result){
+			localData.initialAmount = result.initialAmount;
+		}
+		if ("recurringTransactions" in result){
+			localData.recurringTransactions = result.recurringTransactions;
+		}
+		if ("config" in result){
+			localData.config = result.config;
+		}
+		saveLocalData();
+		console.log(localData.transactions);
+	}
+	reader.readAsText(event.target.files[0]);
+	document.getElementById("transactionsFileInput").value = ""; //so the onchange event works
 }
 
 function exportJSON(){
